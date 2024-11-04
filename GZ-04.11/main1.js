412986a7addf96eea9e7af2a73fa8f9c3e921aee
gultaczaur <gultaczaur@gmail.com>
2024-11-04 19:02:41 +0400

    Add files via upload

---
+++ GZ-04.11/main1.js
@@ -0,0 +1,73 @@
+/*1)
+Bir array yaradin adlardan ibaret olsun hemin arraya her hansisa bir adi
+arrayin metodlarindan isdifade etmekle daxil edin daha sonra silin */
+/* var namee = ["Gültac","Zaur","Əli"]
+console.log(namee);
+namee.push ("Leyla")
+console.log(namee)
+namee.pop ()
+console.log(namee); */
+
+
+
+/* bu arrayde adi n ile baslayanlari loga cixaracaq funksiya qurun*/
+// let obj = [
+//     {
+//         name: "Ali",
+//         surname: "Aliyev",
+//         age: 10,
+//         driving: true
+//     }, {
+//         name: "Veli",
+//         surname: "Aliyev",
+//         age: 17,
+//         driving: false,
+//     }, {
+//         name: "Mehman",
+//         surname: "Aliyev",
+//         age: 25,
+//         driving: true,
+//     }, {
+//         name: "Nicat",
+//         surname: "Aliyev",
+//         age: 26,
+//         driving: false
+//     }, {
+//         name: "Ayxan",
+//         surname: "Aliyev",
+//         age: 15,
+//         driving: true
+//     }, {
+//         name: "Nihad",
+//         surname: "Aliyev",
+//         age: 19,
+//         driving: true
+//     }
+// ]
+
+
+
+/* yasi 20 den boyuk ve 26 den kicik olan objectleri console.loga cixaran funksiya yazin*/
+// function checkAge() {
+//     for (let i=0; i<6; i++) {
+//         if  (obj[i].age >= 20 && obj[i].age <= 26) {
+//             console.log(obj[i]);
+//             }}}
+// checkAge()
+
+
+
+
+/*obj arrayini tersine yazdirin
+yeni baslangic deyer Ali deyil Nihad olsun*/
+// console.log( obj.reverse ());
+
+
+// yasi cut olan ve suruculuk vesieqesi olanlari loga cixaran funksiya yazin
+// function checkAge () {
+ 
+//     for (let i=0; i<6; i++) {
+//         if  (obj[i].age %2 ==0 && obj[i].driving==true) {
+//             console.log(obj[i]);
+//             }} }
+// checkAge()
